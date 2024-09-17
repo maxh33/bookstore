8dbf65dd5cc6b97324815d2bc79f6a90e81ed7a8
maxh33 <contact@maxhaider.dev>
2024-09-17 10:33:14 -0300

    Add tests for Order Category and Product serializers

--- product/models/category.py
+++ product/models/category.py
@@ -3,7 +3,9 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
-    description = models.BooleanField(default=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+
 
     def __unicode__(self):
         return self.title

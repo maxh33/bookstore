37215bce3f8ee6912a59bbee95d4400dafc6746c
maxh33 <contact@maxhaider.dev>
2024-09-25 07:21:09 -0300

    test category viewset

---
+++ product/tests/test_viewsets/test_category_viewset.py
@@ -0,0 +1,41 @@
+import json
+
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+
+from product.factories import CategoryFactory
+from product.models import Category
+
+class CategoryViewSet(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.category = CategoryFactory(title='GPU')
+
+    def test_get_all_category(self):
+        response = self.client.get(
+            reverse('category-list', kwargs={'version': 'v1'})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        category_data = json.loads(response.content)
+
+        self.assertEqual(category_data[0]['title'], self.category.title)
+
+    def test_create_category(self):
+        data = json.dumps({
+            'title': 'CPU',
+        })
+
+        response = self.client.post(
+            reverse('category-list', kwargs={'version': 'v1'}),
+            data=data,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        created_category = Category.objects.get(title='CPU')
+
+        self.assertEqual(created_category.title, 'CPU')
